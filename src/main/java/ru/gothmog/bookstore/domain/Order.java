b14b26b4237e4027498b923b7c30eaca645ff6d8
gothmog3479 <gothmog79@mail.ru>
2017-07-25 15:58:53 +0300

    update Order add field CartItem

--- src/main/java/ru/gothmog/bookstore/domain/Order.java
+++ src/main/java/ru/gothmog/bookstore/domain/Order.java
@@ -3,6 +3,7 @@ package ru.gothmog.bookstore.domain;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author d.grushetskiy
@@ -19,4 +20,9 @@ public class Order {
     private String shippingMethod;
     private String orderStatus;
     private BigDecimal orderTotal;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<CartItem> cartItemList;
+
+    
 }
