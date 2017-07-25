69a8444e4a45be968757a48f60e3203885b704d6
gothmog3479 <gothmog79@mail.ru>
2017-07-25 15:00:18 +0300

    add CartItem

---
+++ src/main/java/ru/gothmog/bookstore/domain/CartItem.java
@@ -0,0 +1,22 @@
+package ru.gothmog.bookstore.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+/**
+ * @author d.grushetskiy
+ */
+@Entity
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private int cty;
+    private BigDecimal subTotal;
+
+    
+}
