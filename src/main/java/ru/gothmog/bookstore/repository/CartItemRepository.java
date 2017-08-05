ac39cdc2c5798da1cb62fcaf25091a5e03adf4d6
Dmitriy <dgrushetskiy@gmail.com>
2017-08-06 01:39:47 +0300

    add interface CartItemRepository

---
+++ src/main/java/ru/gothmog/bookstore/repository/CartItemRepository.java
@@ -0,0 +1,20 @@
+package ru.gothmog.bookstore.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.gothmog.bookstore.domain.CartItem;
+import ru.gothmog.bookstore.domain.Order;
+import ru.gothmog.bookstore.domain.ShoppingCart;
+
+import java.util.List;
+
+/**
+ * @author gothmog on 06.08.2017.
+ */
+@Transactional
+public interface CartItemRepository extends CrudRepository<CartItem, Long> {
+
+    List<CartItem> findByShoppingCart(ShoppingCart shoppingCart);
+
+    List<CartItem> findByOrder(Order order);
+}
