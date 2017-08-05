155d54936da13184087d99e2f9f8192193148253
Dmitriy <dgrushetskiy@gmail.com>
2017-08-06 01:08:04 +0300

    add SecurityInit on Spring Security

---
+++ src/main/java/ru/gothmog/bookstore/config/SecurityInit.java
@@ -0,0 +1,10 @@
+package ru.gothmog.bookstore.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * on Spring Security
+ * @author gothmog on 06.08.2017.
+ */
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
