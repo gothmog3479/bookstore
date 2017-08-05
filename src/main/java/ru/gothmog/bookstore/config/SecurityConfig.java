2d3faf7290fd1f5e7431384b749d468193595237
Dmitriy <dgrushetskiy@gmail.com>
2017-08-06 01:29:54 +0300

    add interface PasswordResetTokenRepository

--- src/main/java/ru/gothmog/bookstore/config/SecurityConfig.java
+++ src/main/java/ru/gothmog/bookstore/config/SecurityConfig.java
@@ -10,4 +10,5 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 }
