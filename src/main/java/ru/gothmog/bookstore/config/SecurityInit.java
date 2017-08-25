9fbb545783aed2e42449afe63f76ad3410e85090
gothmog3479 <gothmog79@mail.ru>
2017-08-25 15:04:57 +0300

    add SecurityInit

---
+++ src/main/java/ru/gothmog/bookstore/config/SecurityInit.java
@@ -0,0 +1,9 @@
+package ru.gothmog.bookstore.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * @author d.grushetskiy
+ */
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer{
+}
