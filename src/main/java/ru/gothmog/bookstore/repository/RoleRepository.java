acf443cdf8944886bd39539df2dc655a7532f5a1
gothmog3479 <gothmog79@mail.ru>
2017-08-25 15:01:38 +0300

    add RoleRepository

---
+++ src/main/java/ru/gothmog/bookstore/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package ru.gothmog.bookstore.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import ru.gothmog.bookstore.domain.security.Role;
+
+/**
+ * @author d.grushetskiy
+ */
+public interface RoleRepository extends CrudRepository<Role, Long>{
+    Role findByname(String name);
+}
