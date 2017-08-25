f6a2851b48c6690a59806bd342e1eb077905a3d1
gothmog3479 <gothmog79@mail.ru>
2017-08-25 14:59:12 +0300

    add ShoppingCartRepository

--- src/main/java/ru/gothmog/bookstore/repository/ShoppingCartRepository.java
+++ src/main/java/ru/gothmog/bookstore/repository/ShoppingCartRepository.java
@@ -1,10 +1,10 @@
 package ru.gothmog.bookstore.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import ru.gothmog.bookstore.domain.UserPayment;
+import ru.gothmog.bookstore.domain.ShoppingCart;
 
 /**
  * @author d.grushetskiy
  */
-public interface UserPaymentRepository extends CrudRepository<UserPayment, Long>{
+public interface ShoppingCartRepository extends CrudRepository<ShoppingCart, Long>{
 }
